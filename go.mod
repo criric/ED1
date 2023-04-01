4e6555cb21a1f6e5b593c965bee170f8d1ff0b35
Cristian <criricyt@gmail.com>
2023-04-01 11:49:46 -0300

    tipos de lista

---
+++ go.mod
@@ -0,0 +1,5 @@
+module list
+
+go 1.20
+
+require github.com/eduardolfalcao/edi v0.0.0-20230328151151-164f131d5e92
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/eduardolfalcao/edi/src/go/list"
+)
+
+func main() {
+	//var linkedlist list.ArrayList
+	linkedlist := list.LinkedList{}
+	limit := 15
+
+	for i := 0; i < limit; i++ {
+		linkedlist.Add(i)
+	}
+	for i := 0; i < limit; i++ {
+		val, err := linkedlist.Get(i)
+		if err == nil {
+			fmt.Println(val)
+		}
+	}
+	for i := 0; i < limit; i++ {
+		if i%2 == 0 {
+			linkedlist.Set(i*2, i)
+		}
+	}
+	fmt.Println("######################")
+	for i := 0; i < limit; i++ {
+		val, err := linkedlist.Get(i)
+		if err == nil {
+			fmt.Println(val)
+		}
+	}
+}
